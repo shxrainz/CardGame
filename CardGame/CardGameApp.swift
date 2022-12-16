e195653263d59530bee550ea8f349773821b7647
Muhamad Syahreen <muhamadsyahreen@gmail.com>
2022-12-16 20:29:25 +0800

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Muhamad Syahreen on 16/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
